6d5c9a4359dbd77e8b49abb44adfdc44261d9b51
matthewrr <matthewrr@users.noreply.github.com>
2018-06-20 16:33:22 +0000

    Create models for homepage sections

--- index/models.py
+++ index/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name =models.CharField(max_length=256)
+    subtitle = models.CharField(max_length=256)
+    description = models.TextField()
+    location = models.CharField(max_length=256)
+    email = models.EmailField()
+    linkedin = models.URLField()
+
+class Education(models.Model):
+    school = models.CharField(max_length=256)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    degree = models.CharField(max_length=256)
+    description = models.TextField()
+    location = models.CharField(max_length=256)
+
+class Experience(models.Model):
+    company = models.CharField(max_length=256)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    location = models.CharField(max_length=256)
+    
+# class Projects(models.Model):
+#     image = models.ImageField()
+#     title = models.CharField(max_length=256)
+#     description = models.TextField()
+    
+class Abilities(models.Model):
+    ABILITIES_CATEGORIES = (('skills','Skills'), ('tools','Tools'))
+    category = models.CharField(max_length=1, choices=ABILITIES_CATEGORIES)
+    skill_level = models.IntegerField()
